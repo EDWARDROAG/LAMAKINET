d0a160a8d10ee4de4c7e1d457fd15dd26815cc1a
Edward <edwardroag@gmail.com>
2024-08-29 11:57:14 -0500

    primer avance

---
+++ js/nuestras_paginas.js
@@ -0,0 +1,15 @@
+/* document.addEventListener('DOMContentLoaded', () => {
+    const accordions = document.querySelectorAll('.accordion');
+
+    accordions.forEach(acc => {
+        acc.addEventListener('click', () => {
+            const panel = acc.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    });
+});
+ */
